399f453f8b93711925c9341e405c8992c6507f0e
thao phuong <dophuongthao.459@gmail.com>
2022-03-25 23:09:07 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Form from './components/Form';
+import Todolist from './components/Todolist';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>tharod's to-do</h1>
       </header>
+      <Form/>
+      <Todolist/>
+
     </div>
   );
 }
---
+++ src/components/Todolist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Todolist() {
+    return (
+        <div className='todo-container'>
+            <ul className='todo-list'></ul>
+        </div>
+    );
+}
+
+export default Todolist;
