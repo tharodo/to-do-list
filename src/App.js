4ced18e1a597d9614a53d95524aeff36b2bc6ef7
thao phuong <dophuongthao.459@gmail.com>
2022-03-25 23:40:21 +0700

    pass setInputText down to Form.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Form from './components/Form';
 import Todolist from './components/Todolist';
 
 function App() {
+
+  //useState pass 'setInputText' down to Form.jsx
+
   const [inputText, setInputText] = useState("");
+  
   return (
     <div className="App">
       <header>
         <h1>tharod's to-do</h1>
       </header>
-      <Form/>
+      <Form setInputText={setInputText} />
       <Todolist/>
 
     </div>
