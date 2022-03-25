94e1d5bd54d956149822c73e89e2d1c45cc3d84b
thao phuong <dophuongthao.459@gmail.com>
2022-03-26 00:36:28 +0700

    useState for todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
-import './App.css';
-import Form from './components/Form';
-import Todolist from './components/Todolist';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Todolist from "./components/Todolist";
 
 function App() {
-
   //useState pass 'setInputText' down to Form.jsx
-
+  //state for Form
   const [inputText, setInputText] = useState("");
-  
+
+  //state for Todolist
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>tharod's to-do</h1>
       </header>
-      <Form setInputText={setInputText} />
-      <Todolist/>
-
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <Todolist />
     </div>
   );
 }
